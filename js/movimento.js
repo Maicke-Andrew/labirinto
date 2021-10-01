8bbbefe02c6a65ffabda57cc2a80b02757af7094
Maicke <maicke.andrew@hotmail.com>
2021-10-01 18:11:06 -0300

    atualizacao

--- js/movimento.js
+++ js/movimento.js
@@ -1,27 +1,46 @@
 let jogador = document.getElementById("jogador");
+let alteracao = document.getElementById("start");
+let parede = document.getElementById("paredes");
+let cursor = document.getElementById("cursor")
+let final = document.getElementById("paredevencedora")
+let buttaowin = document.getElementById("winbutton")
+let win = document.getElementById("win")
+
+function play() {
+    alteracao.style.display = "none";
+    parede.style.display = "block"
+    cursor.style.cursor = "none"
+    final.style.display = "flex"
+}
 
 let onMouseMove = (e) =>{
     jogador.style.left = e.pageX + "px";
     jogador.style.top = e.pageY + "px";
 }
-
 document.addEventListener("mousemove", onMouseMove);
 
-let alteracao = document.getElementById("start");
-let parede = document.getElementById("paredes");
-let cursor = document.getElementById("cursor")
-
-function play() {
-    alteracao.style.display = "none";
-    parede.style.display = "flex"
-    cursor.style.cursor = "none"
+function gameover() {
+    let fimdejogo = document.getElementById("fimdejogo");
+    fimdejogo.style.display = "flex";
+    parede.style.display = "none";
+    cursor.style.cursor = "context-menu"
 }
 
-document.getElementById("paredes").addEventListener("mouseover", gameover)
+parede.addEventListener("mouseover", gameover);
 
-function gameover() {
-   let fimdejogo = document.getElementById("fimdejogo");
-   fimdejogo.style.display = "flex";
-   parede.style.display = "none";
-   cursor.style.cursor = "context-menu"
+function wingame() {
+    win.style.display = "block"
+    buttaowin.style.display = "flex"
+    cursor.style.cursor = "context-menu"
+}
+
+final.addEventListener("mouseover", wingame);
+
+function playagain() {
+    let fimdejogo = document.getElementById("fimdejogo")
+    fimdejogo.style.display = "none"
+    parede.style.display = "flex";
+    cursor.style.cursor = "none"
+    buttaowin.style.display = "none"
+    win.style.display = "none"
 }
